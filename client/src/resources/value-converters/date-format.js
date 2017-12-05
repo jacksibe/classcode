9451e7d37d7184ea2cc2c875cda7ba4983ec3fb4
jacksibe <jacksibe@gmail.com>
2017-12-04 19:16:10 -0600

    final commit

--- client/src/resources/value-converters/date-format.js
+++ client/src/resources/value-converters/date-format.js
@@ -1,6 +1,6 @@
 import moment from 'moment';
 export class DateFormatValueConverter {
-toView(value, format = 'MMM Do YYY') {
+toView(value, format = 'MMM Do YYYY') {
 if(value === undefined || value === null){
 	return;
     		}
